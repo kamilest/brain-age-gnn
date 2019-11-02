5d916dda877ab54e60a59a5fb26b2e582ba10478
kamilest <stankeviciute.kamile@gmail.com>
2019-11-02 14:08:47 +0000

    [G1] Remove stub code.

--- preprocess.py
+++ preprocess.py
@@ -72,63 +72,9 @@ def get_functional_connectivity(timeseries, subject, kind, save=True, save_path=
     The flattened lower triangle of the correlation matrix for the parcellated
     timeseries data.
   """
-  
+
 
   pass
-
-
-# Get phenotype values for a list of subjects
-def get_subject_score(subject_list, score):
-  # scores_dict = {}
-
-  # with open(phenotype) as csv_file:
-  #     reader = csv.DictReader(csv_file)
-  #     for row in reader:
-  #         if row['SUB_ID'] in subject_list:
-  #             scores_dict[row['SUB_ID']] = row[score]
-
-  # return scores_dict
-
-  pass
-
-
-# Construct the adjacency matrix of the population from phenotypic scores
-def create_affinity_graph_from_scores(scores, subject_list):
-  """
-      scores       : list of phenotypic information to be used to construct the affinity graph
-      subject_list : list of subject IDs
-
-  return:
-      graph        : adjacency matrix of the population graph (num_subjects x num_subjects)
-  """
-  pass
-
-  # num_nodes = len(subject_list)
-  # graph = np.zeros((num_nodes, num_nodes))
-
-  # for l in scores:
-  #     label_dict = get_subject_score(subject_list, l)
-
-  #     # quantitative phenotypic scores
-  #     if l in ['AGE_AT_SCAN', 'FIQ']:
-  #         for k in range(num_nodes):
-  #             for j in range(k + 1, num_nodes):
-  #                 try:
-  #                     val = abs(float(label_dict[subject_list[k]]) - float(label_dict[subject_list[j]]))
-  #                     if val < 2:
-  #                         graph[k, j] += 1
-  #                         graph[j, k] += 1
-  #                 except ValueError:  # missing label
-  #                     pass
-
-  #     else:
-  #         for k in range(num_nodes):
-  #             for j in range(k + 1, num_nodes):
-  #                 if label_dict[subject_list[k]] == label_dict[subject_list[j]]:
-  #                     graph[k, j] += 1
-  #                     graph[j, k] += 1
-
-  # return graph
 
 
 subject_ids = get_subject_ids(1)
