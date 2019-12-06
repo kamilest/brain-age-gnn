5fa016078166d6f70efba5ab1c17ea69b63183ac
kamilest <stankeviciute.kamile@gmail.com>
2019-12-06 15:13:13 +0000

    Construct example 100-edge graph.
    So enough both for traning but also small.

--- preprocess.py
+++ preprocess.py
@@ -162,5 +162,5 @@ def load_population_graph(graph_root, name):
 
 
 if __name__ == '__main__':
-    construct_population_graph(5, name='population_graph5.pt')
-    graph = load_population_graph(graph_root, name='population_graph5.pt')
+    construct_population_graph(100, name='population_graph100.pt')
+    graph = load_population_graph(graph_root, name='population_graph100.pt')
