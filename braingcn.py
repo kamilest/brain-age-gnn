4c63f231f010bd5e3ecfd96bb0ecfa9d3e1292f4
kamilest <stankeviciute.kamile@gmail.com>
2019-12-07 16:03:11 +0000

    Implement cross-validation over the entire dataset.

--- braingcn.py
+++ braingcn.py
@@ -11,10 +11,46 @@ from torch_geometric.nn import GCNConv
 
 import numpy as np
 from sklearn.metrics import r2_score
+from sklearn.model_selection import StratifiedKFold
 
 graph_root = 'data/graph'
 graph_name = 'population_graph1000.pt'
 population_graph = preprocess.load_population_graph(graph_root, graph_name)
+
+
+# TODO cross-validate within the training set, not in the entire set.
+def cross_validation_score(data, n_splits=5):
+    X = data.x.cpu().numpy()
+    y = data.y.cpu().numpy()
+
+    skf = StratifiedKFold(n_splits=n_splits, random_state=0)
+    skf.get_n_splits()
+
+    cv_scores = []
+    for train_index, test_index in skf.split(X, y):
+        model = BrainGCN().to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+
+        print('Training fold ', len(cv_scores)+1)
+        # train fold
+        model.train()
+        for epoch in range(150):
+            optimizer.zero_grad()
+            out = model(data)
+            loss = F.mse_loss(out[train_index], data.y[train_index])
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+        final_model = model(data)
+        predicted = final_model[test_index].cpu()
+        actual = data.y[test_index].cpu()
+        r2 = r2_score(actual.detach().numpy(), predicted.detach().numpy())
+        cv_scores.append(r2)
+        print('r2', r2)
+        print('mse', F.mse_loss(predicted, actual))
+
+    return np.mean(cv_scores)
 
 
 class BrainGCN(torch.nn.Module):
@@ -38,28 +74,9 @@ class BrainGCN(torch.nn.Module):
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = BrainGCN().to(device)
 data = population_graph.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 
 torch.manual_seed(0)
 np.random.seed(0)
 
-model.train()
-for epoch in range(150):
-    optimizer.zero_grad()
-    out = model(data)
-    loss = F.mse_loss(out[data.train_mask], data.y[data.train_mask])
-    if epoch % 10 == 9:
-        print(epoch, loss)
-    loss.backward()
-    optimizer.step()
-
-model.eval()
-final_model = model(data)
-predicted = final_model[data.train_mask].cpu()
-actual = data.y[data.train_mask].cpu()
-r2 = r2_score(actual.detach().numpy(), predicted.detach().numpy())
-print(r2)
-mse = F.mse_loss(final_model[data.test_mask], data.y[data.test_mask])
-print(mse)
+print('final mean r2 score', cross_validation_score(data))
