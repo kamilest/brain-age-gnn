cf05016cb4bc55bf327c3702fc8f6a80f6de3404
kamilest <stankeviciute.kamile@gmail.com>
2019-12-13 15:08:38 +0000

    Cleanup.

--- braingcn.py
+++ braingcn.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import StratifiedKFold
 from torch.utils.tensorboard import SummaryWriter
 
 graph_root = 'data/graph'
-graph_name = 'population_graph_1000.pt'
+graph_name = 'population_graph_1000_PCA.pt'
 population_graph = preprocess.load_population_graph(graph_root, graph_name)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -50,7 +50,7 @@ def gcn_train_cv(data, folds=5):
 
 def gcn_train(data):
     writer = SummaryWriter(
-        log_dir='runs/PCA_conv1_fc3_841_1024_512_256_1_tanh_epochs=250_lr=0.005_weight_decay=1e-5')
+        log_dir='runs/PCA_fc2_841_1024_512_1_tanh_epochs=250_lr=0.005_weight_decay=1e-5')
 
     model = BrainGCN().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=1e-5)
@@ -97,29 +97,21 @@ def gcn_train(data):
     print('Final validation r2: {}'.format(r2))
     print('Final validation MSE: {}\n'.format(F.mse_loss(predicted, actual)))
 
-    return r2
+    return r2, predicted, actual
 
 
 class BrainGCN(torch.nn.Module):
     def __init__(self):
         super(BrainGCN, self).__init__()
-        self.conv1 = GCNConv(population_graph.num_node_features, 1024)
-        # self.conv2 = GCNConv(1024, 2048)
-        self.fc_1 = Linear(1024, 512)
-        self.fc_2 = Linear(512, 256)
-        self.fc_3 = Linear(256, 1)
+        # self.conv1 = GCNConv(population_graph.num_node_features, 1024)
+        self.fc_1 = Linear(population_graph.num_node_features, 1024)
+        self.fc_2 = Linear(1024, 1)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-
-        x = self.conv1(x, edge_index)
-        x = torch.tanh(x)
         x = self.fc_1(x)
         x = torch.tanh(x)
-        # x = F.dropout(x, p=0.1, training=self.training)
         x = self.fc_2(x)
-        x = torch.tanh(x)
-        x = self.fc_3(x)
 
         return x
 
@@ -127,4 +119,4 @@ class BrainGCN(torch.nn.Module):
 # torch.manual_seed(0)
 # np.random.seed(0)
 
-r2 = gcn_train(data)
+r2, predicted, actual = gcn_train(data)
