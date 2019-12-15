d4d83c3deb55dc76749d610c9fe502ec0ad42263
kamilest <stankeviciute.kamile@gmail.com>
2019-12-15 15:09:41 +0000

    Implement the xgboost model. f

---
+++ brain_xgboost.py
@@ -0,0 +1,21 @@
+import xgboost as xgb
+import preprocess
+
+from scipy.stats import pearsonr
+from sklearn.metrics import r2_score
+
+graph_root = 'data/graph'
+graph_name = 'population_graph_all_structural_euler_no_edges_sex.pt'
+population_graph = preprocess.load_population_graph(graph_root, graph_name)
+
+X_train = population_graph.x[population_graph.train_mask].cpu().detach().numpy()
+X_validate = population_graph.x[population_graph.validate_mask].cpu().detach().numpy()
+
+y_train = population_graph.y[population_graph.train_mask].cpu().detach().numpy().flatten()
+y_validate = population_graph.y[population_graph.validate_mask].cpu().detach().numpy().flatten()
+
+xgb_model = xgb.XGBRegressor(learning_rate=0.01, n_jobs=-1, verbosity=2)
+
+xgb_model.fit(X_train, y_train, verbose=True)
+print('pearsonr', pearsonr(y_validate, xgb_model.predict(X_validate)))
+print('r2_score', r2_score(y_validate, xgb_model.predict(X_validate)))
