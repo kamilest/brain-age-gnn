3811412889264c27af19d6728f25aea4fc0dff8b
kamilest <stankeviciute.kamile@gmail.com>
2020-01-01 11:50:03 +0200

    Add more phenotype features.

--- phenotype.py
+++ phenotype.py
@@ -7,3 +7,7 @@ class Phenotype(enum.Enum):
     FULL_TIME_EDUCATION = ['845-1.0, 845-2.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=845
     FLUID_INTELLIGENCE = ['20016-0.0', '20016-1.0', '20016-2.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20016
     PROSPECTIVE_MEMORY_RESULT = '20018-2.0'  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20018
+    MENTAL_HEALTH = ['20544-0.' + str(i) for i in range(16)]  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20544
+    BIPOLAR_DISORDER_STATUS = '20122-0.0'  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20122
+    NEUROTICISM_SCORE = '20127-0.0'  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20127
+    SMOKING_STATUS = '20116-2.0'  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20116
