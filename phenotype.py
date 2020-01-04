4c64bb993db6d0858c65ace0380132d8a5b189b4
kamilest <stankeviciute.kamile@gmail.com>
2020-01-04 22:50:40 +0000

    Bug fixes to similarity function code.

--- phenotype.py
+++ phenotype.py
@@ -4,7 +4,7 @@ import enum
 class Phenotype(enum.Enum):
     SEX = ['31-0.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=31
     AGE = ['21003-2.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=21003
-    FULL_TIME_EDUCATION = ['845-1.0, 845-2.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=845
+    FULL_TIME_EDUCATION = ['845-1.0', '845-2.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=845
     FLUID_INTELLIGENCE = ['20016-0.0', '20016-1.0', '20016-2.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20016
     PROSPECTIVE_MEMORY_RESULT = ['20018-2.0']  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20018
     MENTAL_HEALTH = ['20544-0.' + str(i) for i in range(1, 17)]  # http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=20544
