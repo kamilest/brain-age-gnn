25a17802c3e4cdc5b15419d245837c9734a2fb19
kamilest <stankeviciute.kamile@gmail.com>
2020-01-05 16:43:41 +0000

    PEP257 docstrings.

--- evaluate.py
+++ evaluate.py
@@ -1,29 +1,51 @@
 
-def add_population_graph_noise(graph, noise):
+def add_population_graph_noise(graph, p, noise_amplitude):
+    """Adds white Gaussian noise to the nodes of the population graph.
+
+    :param graph: path to the population graph file.
+    :param p: probability of adding noise.
+    :param noise_amplitude: the variance of white noise.
+    :return: the modified graph with increased noise.
     """
-    Noisifies the graph
-    :param graph:
-    :param noise:
-    :return:
-    """
+
     pass
 
 
-def add_population_graph_errors(graph, error_size, error_frequency):
+def remove_population_graph_edges(graph, p):
+    """Removes graph edges with probability p.
+
+    :param graph: path to the population graph file.
+    :param p: probability of removing the edge.
+    :return: the modified graph with fewer edges.
     """
 
-    :param graph:
-    :param error_size:
-    :param error_frequency:
+    pass
+
+
+def add_population_graph_edge_errors(graph, p):
+    """Changes the graph connectivity by adding or removing edges.
+
+    :param graph: path to the population graph file.
+    :param p: probability of error (adding or removing an edge).
+    :return: the modified graph with edge errors.
+    """
+
+    pass
+
+
+def decrease_population_graph_train_set(graph, test_set_sizes):
+    """Decreases the training set (more unlabeled nodes).
+
+    :param graph: path to the population graph file.
+    :param test_set_sizes:
     :return:
     """
-    pass
 
 
 def measure_predictive_power_drop():
-    """
+    """Measures the drop in performance metrics with increased noise or more missing data.
 
-    :return:
+    :return: the range of values at different modification levels.
     """
     pass
 
