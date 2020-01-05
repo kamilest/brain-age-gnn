eadb777b2e192d05c9ba45aaed6c0a25ea375290
kamilest <stankeviciute.kamile@gmail.com>
2020-01-05 13:53:41 +0000

    Initial evaluation framework commit.

---
+++ evaluate.py
@@ -0,0 +1,29 @@
+
+def add_population_graph_noise(graph, noise):
+    """
+    Noisifies the graph
+    :param graph:
+    :param noise:
+    :return:
+    """
+    pass
+
+
+def add_population_graph_errors(graph, error_size, error_frequency):
+    """
+
+    :param graph:
+    :param error_size:
+    :param error_frequency:
+    :return:
+    """
+    pass
+
+
+def measure_predictive_power_drop():
+    """
+
+    :return:
+    """
+    pass
+
