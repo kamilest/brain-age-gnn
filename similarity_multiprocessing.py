dee94c8c4d0cf23ca08f14885224157c614e9a1c
kamilest <stankeviciute.kamile@gmail.com>
2020-03-07 13:18:21 +0000

    Boolean comparison in similarity_multiprocessing.

--- similarity_multiprocessing.py
+++ similarity_multiprocessing.py
@@ -27,20 +27,20 @@ def precompute_mental_similarities(n):
 
     sm = []
     mental_feature_codes = [Phenotype.MENTAL_HEALTH.value + str(i) for i in range(1, 19)]
-    for i in range(2**n+1, min(2**(n+1)+1, 130)):
+    for i in range(132 / 10 * n, min(132 / 10 * (n+1) + 1, 132)):
         id_i = subject_ids[i]
-        for j in range(i+1, min(2**(n+1)+1, 130)):
+        for j in range(i+1, min(132 / 10 * (n+1) + 1, 132)):
             id_j = subject_ids[j]
-            sm.append([i, j, np.dot(similarity_lookup.loc[id_i, mental_feature_codes],
-                                    similarity_lookup.loc[id_j, mental_feature_codes]) >= 1])
+            sm.append([i, j, np.any(np.logical_and(similarity_lookup.loc[id_i, mental_feature_codes],
+                                                   similarity_lookup.loc[id_j, mental_feature_codes]))])
     return np.array(sm)
 
 
 def main():
     start = time.clock()
 
-    num_processes = 14  # log_2(17314)
-    thread_id = list(range(num_processes))
+    num_processes = 10
+    thread_id = list(range(num_processes+1))
 
     out = run_multiprocessing(precompute_mental_similarities, thread_id, num_processes)
     np.save(os.path.join(similarity_root, 'MEN_multiprocess_similarity'), np.array(out))
