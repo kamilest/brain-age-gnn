485c219e9f7a2843987e263fff982620ce6c74f9
kamilest <stankeviciute.kamile@gmail.com>
2020-03-07 15:21:07 +0000

    Split similarity computation into threads, concatenate results.

--- similarity_multiprocessing.py
+++ similarity_multiprocessing.py
@@ -15,10 +15,11 @@ similarity_root = 'data/similarity'
 SIMILARITY_LOOKUP = 'data/similarity_lookup.pkl'
 SUBJECT_IDS = 'data/subject_ids.npy'
 
+NUM_PROCESSES = 256
 
 def run_multiprocessing(func, i, n_processors):
     with Pool(processes=n_processors) as pool:
-        return np.array(pool.map(func, i)).flatten()
+        return np.concatenate(np.array(pool.map(func, i)).flatten())
 
 
 def precompute_mental_similarities(n):
@@ -27,9 +28,13 @@ def precompute_mental_similarities(n):
 
     sm = []
     mental_feature_codes = [Phenotype.MENTAL_HEALTH.value + str(i) for i in range(1, 19)]
-    for i in range(132 / 10 * n, min(132 / 10 * (n+1) + 1, 132)):
+    lo = int(len(subject_ids) / NUM_PROCESSES) * n
+    hi = min(int(len(subject_ids) / NUM_PROCESSES) * (n+1), len(subject_ids))
+    # print('Processing {}-{}/{}'.format(lo, hi, len(subject_ids)))
+    for i in range(lo, hi):
+        print('Subject {}'.format(i))
         id_i = subject_ids[i]
-        for j in range(i+1, min(132 / 10 * (n+1) + 1, 132)):
+        for j in range(i+1, len(subject_ids)):
             id_j = subject_ids[j]
             sm.append([i, j, np.any(np.logical_and(similarity_lookup.loc[id_i, mental_feature_codes],
                                                    similarity_lookup.loc[id_j, mental_feature_codes]))])
@@ -39,7 +44,7 @@ def precompute_mental_similarities(n):
 def main():
     start = time.clock()
 
-    num_processes = 10
+    num_processes = NUM_PROCESSES
     thread_id = list(range(num_processes+1))
 
     out = run_multiprocessing(precompute_mental_similarities, thread_id, num_processes)
@@ -50,3 +55,4 @@ def main():
 if __name__ == "__main__":
     freeze_support()
     main()
+    # a = np.load('data/similarity/MEN_multiprocess_similarity.npy', allow_pickle=True)
