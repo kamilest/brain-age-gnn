48a0d0e068305177e9c1dd6c8cec94a795ca7bde
kamilest <stankeviciute.kamile@gmail.com>
2020-03-08 14:54:47 +0000

    wandb default hyperparameters.

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import argparse
 import os
+from datetime import datetime
+from pathlib import Path
 
 import numpy as np
 import torch
+import wandb
 
 import brain_gcn
 import evaluate
@@ -11,20 +14,26 @@ import preprocess
 graph_root = 'data/graph'
 GRAPH_NAMES = sorted(os.listdir(graph_root))
 
+logdir = './runs/{}'.format(datetime.now().strftime('%Y-%m-%d'))
+Path(logdir).mkdir(parents=True, exist_ok=True)
+
+# ARGUMENT PARSING
+
 parser = argparse.ArgumentParser(description='Brain age graph neural network.')
-parser.add_argument('--graph_index', default=0, type=int, help='Graph construction (as index to predefined name array)')
+# parser.add_argument('--graph_index', default=0, type=int, help='Graph construction (as index to predefined name array)')
+parser.add_argument('--graph_name', default=GRAPH_NAMES[0], type=str, help='Graph structure')
 parser.add_argument('--model', default='gcn', type=str, help='Type of model (options: gcn, fc)')
 parser.add_argument('--epochs', default=5000, type=int, help='Number of epochs (default 5000)')
 parser.add_argument('--learning_rate', default=5e-4, type=float, help='Learning rate (default 5e-4)')
-parser.add_argument('--weight_decay', default=1e-5, type=float, help='Weight decay (default 1e-5)')
+parser.add_argument('--weight_decay', default=0, type=float, help='Weight decay (default 0)')
 
 parser.add_argument('--n_conv_layers', default=1, type=int, help='Number of graph convolutional layers (default: 1)')
-# TODO correctly parse layer sizes
 parser.add_argument('--layer_sizes', default='[364, 364, 512, 256, 1]', type=str, help='Sizes of layers')
 
 args = parser.parse_args()
 graph_index = args.graph_index
-graph_name = GRAPH_NAMES[graph_index]
+# graph_name = GRAPH_NAMES[graph_index]
+graph_name = args.graph_name
 
 if args.model == 'gcn' or args.model == 'gat':
     n_conv_layers = args.n_conv_layers
@@ -42,9 +51,24 @@ preprocess.graph_feature_transform(population_graph)
 population_graph = preprocess.load_population_graph(graph_root, graph_name)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+
+hyperparameter_defaults = dict(
+    graph_name=GRAPH_NAMES[0],
+    model='gcn',
+    epochs=5000,
+    learning_rate=5e-4,
+    weight_decay=0,
+    n_conv_layers=1,
+    layer_sizes=[364, 364, 512, 256, 1]
+)
+
+wandb.init(project="brain-age-gnn", config=hyperparameter_defaults)
+
+
 # TODO correctly parse layer sizes
-brain_gcn.gcn_train(population_graph, device, n_conv_layers=n_conv_layers,
-                    layer_sizes=args.layer_sizes,
+brain_gcn.gcn_train(population_graph, device,
+                    n_conv_layers=n_conv_layers,
+                    layer_sizes=[364, 364, 512, 256, 1],
                     lr=args.learning_rate,
                     weight_decay=args.weight_decay,
                     epochs=args.epochs)
