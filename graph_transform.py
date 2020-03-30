7e993bd8d230a86f3c0fe7e60050b74a135716bc
kamilest <stankeviciute.kamile@gmail.com>
2020-03-30 22:42:19 +0300

    graph_transform documentation.

--- graph_transform.py
+++ graph_transform.py
@@ -4,15 +4,32 @@ import torch
 
 
 def functional_connectivities_pca(connectivities, train_idx, random_state=0):
+    """Runs principal component analysis (PCA) on a set of flattened connectivity matrices.
+    Fits to the training set and applies to the rest.
+
+    :param connectivities: flattened functional connectivity matrices.
+    :param train_idx: the boolean mask of the functional connectivity matrices belonging to the trainng set.
+    :param random_state: random state of the PCA transform.
+    :return the principal components of the functional connectivity matrices.
+    """
+
     connectivity_pca = sklearn.decomposition.PCA(random_state=random_state)
     connectivity_pca.fit(connectivities[train_idx])
     return connectivity_pca.transform(connectivities)
 
 
-def graph_feature_transform(population_graph):
+def graph_feature_transform(population_graph, pca=True):
+    """Prepares intermediate population graph with assigned training masks for training.
+    Normalises and concatenates features, optionally running principal component analysis on functional MRI data.
+
+    :param population_graph: population graph object with the assigned training validation and test masks.
+    :param pca: indicates whether to run principal component analysis on functional MRI data.
+    :return prepared population graph with updated feature tensor.
+    """
+
     # Optional functional data preprocessing (PCA) based on the traning index.
     train_mask = population_graph.train_mask.numpy()
-    if not population_graph.functional_data.empty:
+    if not population_graph.functional_data.empty and pca:
         functional_data = functional_connectivities_pca(population_graph.functional_data, train_mask)
     else:
         functional_data = population_graph.functional_data
