4b1fdd8b3718fc64769f74b50c5c5e2fe8447b1c
kamilest <stankeviciute.kamile@gmail.com>
2020-04-12 22:50:51 +0300

    Fix a single graph for archtecture sweep

--- train_gnn.py
+++ train_gnn.py
@@ -71,7 +71,7 @@ fold = gnn_train_evaluate.get_stratified_subject_split(population_graph)
 gnn_train_evaluate.set_training_masks(population_graph, *fold)
 graph_transform.graph_feature_transform(population_graph)
 
-device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
 brain_gnn_train.gnn_train_with_cross_validation(args.model, population_graph, device,
                                                 n_conv_layers=n_conv_layers,
